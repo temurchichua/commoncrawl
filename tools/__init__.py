fbb76d993f6e4620638adce137a35afc7b2d6708
Temur Chichua <69351709+temurchichua@users.noreply.github.com>
2021-03-09 01:45:08 +0400

    again

--- tools/__init__.py
+++ tools/__init__.py
@@ -149,4 +149,20 @@ def main_loop(month_index, pool):
 
 if __name__ == "__main__":
     #  parse_index_file_by_language("D:/PycharmProjects/commoncrawl/data/2021-04/cdx-00000")
-    file_url = "https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2021-04/cc-index.paths.gz"
+    # file_url = "https://commoncrawl.s3.amazonaws.com/crawl-data/CC-MAIN-2021-04/cc-index.paths.gz"
+    start = time.time()
+    tqdm.write(start)
+    line = '0,123,172,167)/index.php/legales 20210117150147 {"url": "https://167.172.123.0/index.php/legales/", ' \
+           '"mime": "text/html", "mime-detected": "text/html", "status": "200", "digest": ' \
+           '"A7ZADSTR36UGBGR5Z5UUTX73OBHDGCVK", "length": "31415", "offset": "191660846", "filename": ' \
+           '"crawl-data/CC-MAIN-2021-04/segments/1610703513062.16/warc/CC-MAIN-20210117143625-20210117173625' \
+           '-00548.warc.gz", "charset": "UTF-8", "languages": "spa"} '
+
+    index = json.loads('{"url":' + line.split('{"url":')[1])
+    tqdm.write(index)
+
+    url = BASE_URL + index['filename']
+    get_wet(url, index['digest'])
+
+    end = time.time()
+    tqdm.write(str(end - start))
